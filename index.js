bbe2adf7f3ea36ed5ed49c4c3214086113187405
DenOlijf <o.vdn@outlook.be>
2021-07-16 14:49:21 +0200

    heroku deploy backend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = Express()
 app.use(Express.json())
 app.use(Express.urlencoded({extended: true}))
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
