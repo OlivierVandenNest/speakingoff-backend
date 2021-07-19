67b4b96e1673d927b58d05c0a056a2a2ae0ece6f
DenOlijf <o.vdn@outlook.be>
2021-07-19 21:01:02 +0200

    change port

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import Express from "express"
-import * as Topic from './topic.js'
-import cors from 'cors'
+import Express from "express";
+import * as Topic from "./topic.js";
+import cors from "cors";
 
-const app = Express()
+const app = Express();
 
-app.use(Express.json())
-app.use(Express.urlencoded({extended: true}))
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 
 // Add headers
 app.use(cors());
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.get('/topics', (req, res) => {
-    res.json(Topic.allTopics)
-})
+app.get("/topics", (req, res) => {
+    res.json(Topic.allTopics);
+});
 
-app.get('/topic/:topicid', (req, res) => {
-    const t = Topic.getTopic(req.params.topicid)
+app.get("/topic/:topicid", (req, res) => {
+    const t = Topic.getTopic(req.params.topicid);
 
-    t.then(value => {res.json(value)}).catch(err => {res.send('Something went wrong: ' + err)})
-})
+    t.then((value) => {
+        res.json(value);
+    }).catch((err) => {
+        res.send("Something went wrong: " + err);
+    });
+});
 
-app.post('/addtopic', (req, res) => {
+app.post("/addtopic", (req, res) => {
     Topic.addTopic(req.body).catch((err) => {
-        res.send(err)
-    })
-    console.log(Topic.allTopics)
-})
+        res.send(err);
+    });
+    console.log(Topic.allTopics);
+});
 
-app.listen(port, () => console.log('listening on port ' + port))
+app.listen(port, () => console.log("listening on port " + port));
